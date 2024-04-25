0a284d9040e4c2e80c649a67cf0621cd999dcfa5
Chloe Terwagne <chloe.terwagne@live.be>
2024-04-25 23:23:42 +0100

    adding final touch

---
+++ assets/styling.css.py
@@ -0,0 +1,8 @@
+.custom-text {
+    font-family: Arial !important;
+    font-size: 15px !important;
+    color: rgb(172, 20, 90) !important; /* yel */
+    text-align: center;
+    position: relative;
+    z-index: 1;
+}
